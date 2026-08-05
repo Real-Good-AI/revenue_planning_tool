f2beeb84f554f44e395217fc6e4a1e787b1c2d96
Mayleen Cortez <43624631+mayscortez@users.noreply.github.com>
2026-08-05 13:21:18 -0700

    added option to specify the directory to save the files to
    - default directory is CORE to be compatible with revenue prediction project
    - also updated logic so that directories are only created if they don't already exist
    - also added one line of logic inside the for loop so that if PF and 501CE are specified it gets skipped since that particular combo does not exist

--- SCRIPTS/download_data.R
+++ SCRIPTS/download_data.R
@@ -5,40 +5,40 @@ library(curl)
 ###########################################################################################################################
 # Step 1(a): Download desired data files from NCCS Core Data Catalog: https://nccs.urban.org/nccs/catalogs/catalog-core.html
 ###########################################################################################################################
-download_CORE <- function(tscope_values, fscope_values, year_values){
-  dir.create("CORE")
-  setwd("CORE")
-  
-  dir.create("pz")
-  dir.create("pc")
-  dir.create("pf")
-  
-  keys <- c("501c3-pz", "501c3-pc", "501ce-pz", "501ce-pc", "501c3-pf")
-  values <- c("-501C3-CHARITIES-PZ-HRMN.csv", "-501C3-CHARITIES-PC-HRMN.csv", "-501CE-NONPROFIT-PZ-HRMN.csv", "-501CE-NONPROFIT-PC-HRMN.csv", "-501C3-PRIVFOUND-PF-HRMN-V0.csv")
-  filename_dict <- hash(keys, values) # dictionary returning the file name convention determined by tscope and fscope
-  base_url     <- "https://nccsdata.s3.amazonaws.com/harmonized/core/"
-  
-  for (tscope in tscope_values){
-      for (fscope in fscope_values){
-          for (year in year_values){
-              # PC only collected starting in 2012; if year is before 2012, skip this iteration
-              if (fscope == "-pc" & year < 2012){
-                  next
-              }
-              t_and_f_scope <- paste(tscope, fscope, sep="")
-              filename <- paste("CORE-", year, filename_dict[[t_and_f_scope]], sep = "")
-              # slightly different URL depending on whether fscope is pf
-              if (fscope != "-pf"){
-                  full_url <- paste(base_url, t_and_f_scope, "/", filename, sep = "")
-              } else {
-                  full_url <- paste(base_url, t_and_f_scope, "/marts/", filename, sep = "")
-              }
-          dest_path <- paste(substrRight(fscope, 2), "/", filename, sep="")
-          download.file( url=full_url, destfile=dest_path, method="curl" )
-          print(paste("Downloaded", filename, sep = " "))
-          }
-      }    
-  }
+download_CORE <- function(tscope_values, fscope_values, year_values, directory = "CORE"){
+      if (!dir.exists(directory)) {dir.create(directory)}
+      setwd(directory)
+      
+      if (!dir.exists("pz")) {dir.create("pz")}
+      if (!dir.exists("pc")) {dir.create("pc")}
+      if (!dir.exists("pf")) {dir.create("pf")}
+      
+      keys <- c("501c3-pz", "501c3-pc", "501ce-pz", "501ce-pc", "501c3-pf")
+      values <- c("-501C3-CHARITIES-PZ-HRMN.csv", "-501C3-CHARITIES-PC-HRMN.csv", "-501CE-NONPROFIT-PZ-HRMN.csv", "-501CE-NONPROFIT-PC-HRMN.csv", "-501C3-PRIVFOUND-PF-HRMN-V0.csv")
+      filename_dict <- hash(keys, values) # dictionary returning the file name convention determined by tscope and fscope
+      base_url     <- "https://nccsdata.s3.amazonaws.com/harmonized/core/"
+      
+      for (tscope in tscope_values){
+            for (fscope in fscope_values){
+                  for (year in year_values){
+                        # PC only collected starting in 2012; if year is before 2012, skip this iteration
+                        if (fscope == "-pc" & year < 2012){next}
+                        # PF files only have 501c3
+                        if (fscope == "-pf" & tscope == "501ce"){next}
+                        t_and_f_scope <- paste(tscope, fscope, sep="")
+                        filename <- paste("CORE-", year, filename_dict[[t_and_f_scope]], sep = "")
+                        # slightly different URL depending on whether fscope is pf
+                        if (fscope != "-pf"){
+                              full_url <- paste(base_url, t_and_f_scope, "/", filename, sep = "")
+                        } else {
+                              full_url <- paste(base_url, t_and_f_scope, "/marts/", filename, sep = "")
+                        }
+                        dest_path <- paste(substrRight(fscope, 2), "/", filename, sep="")
+                        download.file( url=full_url, destfile=dest_path, method="curl" )
+                        print(paste("Downloaded", filename, sep = " "))
+                  }
+            }    
+      }
 }
 
 substrRight <- function(x, n){
